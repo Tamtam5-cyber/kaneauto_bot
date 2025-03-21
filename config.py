fd70171a4388386a103735883993c0ab930c066a
kane5 <Nguyenkhactam5@gmail.com>
2025-03-21 19:10:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-API_ID = "YOUR_API_ID"
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = "24090485"
+API_HASH = "b056e6499bc0d4a81ab375773ac1170c"
+BOT_TOKEN = "7695124221:AAGhrm4zaIeMwtipSPqa_44Pq4gw9ZF4668"
 
 # Danh sách tài khoản người dùng (sử dụng Telethon)
 USER_ACCOUNTS = [
-    {"phone": "+1234567890", "session": "session_1"},
-    {"phone": "+0987654321", "session": "session_2"},
+    {"phone": "+0928609815", "session": "session_1"},
+    {"phone": "+0777710305", "session": "session_2"},
 ]
 
 # Nhóm nguồn và mục tiêu để chuyển tiếp
-SOURCE_CHAT_IDS = [-1001234567890]  # ID kênh hoặc nhóm nguồn
-TARGET_CHAT_IDS = [-1009876543210]  # ID kênh hoặc nhóm đích
+SOURCE_CHAT_IDS = [-1002322003799]  # ID kênh hoặc nhóm nguồn
+TARGET_CHAT_IDS = [-1004639637903]  # ID kênh hoặc nhóm đích
