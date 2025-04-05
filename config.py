af46e074bec2ac131e2212fa8e9bfeafd5d6f743
kane5 <Nguyenkhactam5@gmail.com>
2025-04-06 05:16:42 +0700

    config.py

--- config.py
+++ config.py
@@ -1,13 +1,33 @@
-API_ID = "24090485"
-API_HASH = "b056e6499bc0d4a81ab375773ac1170c"
-BOT_TOKEN = "7695124221:AAGhrm4zaIeMwtipSPqa_44Pq4gw9ZF4668"
+import motor.motor_asyncio
 
-# Danh sách tài khoản người dùng (sử dụng Telethon)
-USER_ACCOUNTS = [
-    {"phone": "+0928609815", "session": "session_1"},
-    {"phone": "+0777710305", "session": "session_2"},
-]
+class Database:
+    def __init__(self, uri, db_name):
+        self.client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self.client[db_name]
+        self.users = self.db.users
+        self.userbots = self.db.userbots
+        self.forwards = self.db.forwards
 
-# Nhóm nguồn và mục tiêu để chuyển tiếp
-SOURCE_CHAT_IDS = [-1002322003799]  # ID kênh hoặc nhóm nguồn
-TARGET_CHAT_IDS = [-1004639637903]  # ID kênh hoặc nhóm đích
+    async def add_userbot(self, user_id, session_string):
+        if not await self.is_userbot_exist(user_id):
+            await self.userbots.insert_one({"user_id": user_id, "session": session_string})
+
+    async def is_userbot_exist(self, user_id):
+        return bool(await self.userbots.find_one({"user_id": user_id}))
+
+    async def get_userbots(self):
+        return [bot async for bot in self.userbots.find({})]
+
+    async def update_forward(self, user_id, details):
+        await self.forwards.update_one({"user_id": user_id}, {"$set": {"details": details}}, upsert=True)
+
+    async def get_forward_details(self, user_id):
+        default = {"source_chats": [], "target_chats": [], "last_id": {}, "fetched": 0}
+        user = await self.forwards.find_one({"user_id": user_id})
+        return user.get("details", default) if user else default
+
+    async def add_forward_config(self, user_id, source_chats, target_chats):
+        details = await self.get_forward_details(user_id)
+        details["source_chats"] = list(set(details["source_chats"] + source_chats))
+        details["target_chats"] = list(set(details["target_chats"] + target_chats))
+        await self.update_forward(user_id, details)
